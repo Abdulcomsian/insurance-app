41499830e83dedcb3a50d6eb6d730517b89e2d81
Abdul Basit <Abdul.Basit@accrualgroup.com>
2021-08-02 14:14:55 +0000

    Updated edit.blade.php

--- resources/views/insurance_companies/edit.blade.php
+++ resources/views/insurance_companies/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.master')
+@extends('layouts.master', ["page_title"=>"Insurance Companies"])
 @section('content')
     <!--begin::Content-->
     <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
