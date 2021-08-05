72685aa959b4177422128713a64013abe97146d6
Alam-Hamza <Hamza.Alam@accrualgroup.com>
2021-08-05 09:09:50 +0500

    changes

--- resources/views/insurance_companies/index.blade.php
+++ resources/views/insurance_companies/index.blade.php
@@ -31,11 +31,11 @@
                             </div>
                             <!--end::Toolbar-->
                             <!--begin::Group actions-->
-                            <div class="d-flex justify-content-end align-items-center d-none" data-kt-user-table-toolbar="selected">
+                            <!-- <div class="d-flex justify-content-end align-items-center d-none" data-kt-user-table-toolbar="selected">
                                 <div class="fw-bolder me-5">
                                     <span class="me-2" data-kt-user-table-select="selected_count"></span>Selected</div>
                                 <button type="button" class="btn btn-danger" data-kt-user-table-select="delete_selected">Delete Selected</button>
-                            </div>
+                            </div> -->
                             <!--end::Group actions-->
                         </div>
 <!--end::Card toolbar-->
