add1c9659c43b948a86b03f065467d5b7196354b
Assad Yaqoob <assad.yaqoob@accrualgroup.com>
2021-08-12 14:56:05 +0500

    update with transaction and edit package

--- resources/views/payment_transactions/show.blade.php
+++ resources/views/payment_transactions/show.blade.php
@@ -1,151 +1,72 @@
 @extends('layouts.master', ["page_title"=>"Payment Transactions"])
-@section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="text-xs-center">
-                    <i class="fa fa-search-plus float-xs-left icon"></i>
-                    <h2>Invoice for purchase #33221</h2>
+@section('css')
+@endsection
+@section('content')<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="text-center">
+{{--                <i class="fa fa-search-plus pull-left icon"></i>--}}
+                <h2>Transaction #{{$transaction->cart_id}}</h2>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-md-6 col-lg-6">
+                    <div class="panel panel-default height">
+                        <div class="panel-heading">Package Information</div>
+                        <div class="panel-body">
+                            <strong>Name: </strong> {{$transaction->package_name}}<br>
+                            <strong>Sanctions: </strong> {{$transaction->package_sanctions}}<br>
+                            <strong>Price: </strong> {{$transaction->package_price}}<br>
+                        </div>
+                    </div>
                 </div>
-                <hr>
-                <div class="row">
-                    <div class="col-xs-12 col-md-3 col-lg-3 float-xs-left">
-                        <div class="card  height">
-                            <div class="card-header">Billing Details</div>
-                            <div class="card-block">
-                                <strong>David Peere:</strong><br>
-                                1111 Army Navy Drive<br>
-                                Arlington<br>
-                                VA<br>
-                                <strong>22 203</strong><br>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-md-3 col-lg-3">
-                        <div class="card  height">
-                            <div class="card-header">Payment Information</div>
-                            <div class="card-block">
-                                <strong>Card Name:</strong> Visa<br>
-                                <strong>Card Number:</strong> ***** 332<br>
-                                <strong>Exp Date:</strong> 09/2020<br>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-md-3 col-lg-3">
-                        <div class="card  height">
-                            <div class="card-header">Order Preferences</div>
-                            <div class="card-block">
-                                <strong>Gift:</strong> No<br>
-                                <strong>Express Delivery:</strong> Yes<br>
-                                <strong>Insurance:</strong> No<br>
-                                <strong>Coupon:</strong> No<br>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-md-3 col-lg-3 float-xs-right">
-                        <div class="card  height">
-                            <div class="card-header">Shipping Address</div>
-                            <div class="card-block">
-                                <strong>David Peere:</strong><br>
-                                1111 Army Navy Drive<br>
-                                Arlington<br>
-                                VA<br>
-                                <strong>22 203</strong><br>
-                            </div>
+                <div class="col-xs-12 col-md-6 col-lg-6">
+                    <div class="panel panel-default height">
+                        <div class="panel-heading">User Information</div>
+                        <div class="panel-body">
+                            <strong>Id: </strong>{{$transaction->user_id}}<br>
+                            <strong>Name: </strong>{{$transaction->username}}<br>
+                            <strong>Email: </strong> {{$transaction->email}}<br>
+                            <strong>Reg Date: </strong> {{$transaction->reg_date}}<br>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card ">
-                    <div class="card-header">
-                        <h3 class="text-xs-center"><strong>Order summary</strong></h3>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-md-6 col-lg-6 pull-left">
+                    <div class="panel panel-default height">
+                        <div class="panel-heading">Billing Details</div>
+                        <div class="panel-body">
+                            <strong>Name: </strong> {{$transaction->billing_fname.' '.$transaction->billing_sname}}:<br>
+                            <strong>Email: </strong>{{$transaction->billing_email ?: '-'}}<br>
+                            <strong>Address 1: </strong>{{$transaction->billing_address_1 ?: '-'}}<br>
+                            <strong>Address 2: </strong>{{$transaction->billing_address_2 ?: '-'}}<br>
+                            <strong>City: </strong>{{$transaction->billing_city ?: '-'}}<br>
+                            <strong>Country: </strong>@isset($transaction->billing_country) {{\App\Utils\TransactionCountries::List[strtolower($transaction->billing_country)]}} @endisset<br>
+                            <strong>Region: </strong>{{$transaction->billing_region ?: '-'}}<br>
+                            <strong>Zip Code: </strong>{{$transaction->billing_zip ?: '-'}}<br>
+                        </div>
                     </div>
-                    <div class="card-block">
-                        <div class="table-responsive">
-                            <table class="table table-sm">
-                                <thead>
-                                <tr>
-                                    <td><strong>Item Name</strong></td>
-                                    <td class="text-xs-center"><strong>Item Price</strong></td>
-                                    <td class="text-xs-center"><strong>Item Quantity</strong></td>
-                                    <td class="text-xs-right"><strong>Total</strong></td>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>Samsung Galaxy S5</td>
-                                    <td class="text-xs-center">$900</td>
-                                    <td class="text-xs-center">1</td>
-                                    <td class="text-xs-right">$900</td>
-                                </tr>
-                                <tr>
-                                    <td>Samsung Galaxy S5 Extra Battery</td>
-                                    <td class="text-xs-center">$30.00</td>
-                                    <td class="text-xs-center">1</td>
-                                    <td class="text-xs-right">$30.00</td>
-                                </tr>
-                                <tr>
-                                    <td>Screen protector</td>
-                                    <td class="text-xs-center">$7</td>
-                                    <td class="text-xs-center">4</td>
-                                    <td class="text-xs-right">$28</td>
-                                </tr>
-                                <tr>
-                                    <td class="highrow"></td>
-                                    <td class="highrow"></td>
-                                    <td class="highrow text-xs-center"><strong>Subtotal</strong></td>
-                                    <td class="highrow text-xs-right">$958.00</td>
-                                </tr>
-                                <tr>
-                                    <td class="emptyrow"></td>
-                                    <td class="emptyrow"></td>
-                                    <td class="emptyrow text-xs-center"><strong>Shipping</strong></td>
-                                    <td class="emptyrow text-xs-right">$20</td>
-                                </tr>
-                                <tr>
-                                    <td class="emptyrow"><i class="fa fa-barcode iconbig"></i></td>
-                                    <td class="emptyrow"></td>
-                                    <td class="emptyrow text-xs-center"><strong>Total</strong></td>
-                                    <td class="emptyrow text-xs-right">$978.00</td>
-                                </tr>
-                                </tbody>
-                            </table>
+                </div>
+                <div class="col-xs-12 col-md-6 col-lg-6">
+                    <div class="panel panel-default height">
+                        <div class="panel-heading">Payment Information</div>
+                        <div class="panel-body">
+                            <strong>Order#: </strong ><span style="word-wrap: break-word">{{$transaction->order_id ?: '-'}} </span> <br>
+                            <strong>Status: </strong> <span class=" badge @if($transaction->status == "Paid")badge-success @else badge-danger @endif">{{$transaction->status ?: '-'}}</span> <br>
+                            <strong>Amount: </strong> {{$transaction->amount ?: '-'}} <br>
+                            <strong>Description: </strong> {{$transaction->description ?: '-'}} <br>
+                            <strong>Card First6: </strong> {{$transaction->card_first6 ?: '-'}} <br>
+                            <strong>Card Last4: </strong> {{$transaction->card_last4 ?: '-'}} <br>
+                            <strong>Card Type: </strong> {{$transaction->card_type ?: '-'}} <br>
+                            <strong>Date: </strong> {{$transaction->created_at ?: '-'}} <br>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <style>
-        .height {
-            min-height: 200px;
-        }
-
-        .icon {
-            font-size: 47px;
-            color: #5CB85C;
-        }
-
-        .iconbig {
-            font-size: 77px;
-            color: #5CB85C;
-        }
-
-        .table > tbody > tr > .emptyrow {
-            border-top: none;
-        }
-
-        .table > thead > tr > .emptyrow {
-            border-bottom: none;
-        }
-
-        .table > tbody > tr > .highrow {
-            border-top: 3px solid;
-        }
-    </style>
+</div>
 
 @endsection
